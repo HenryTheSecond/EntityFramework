a0c0ef6ed152d107bcb23190b4280c7cf83bf1fe
Tuyen Vu Cong <intern.vctuyen@tma.com.vn>
2022-10-27 17:20:32 +0700

    first commit

---
+++ EntityFramework/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (EF_Demo_DBEntities DBEntities = new EF_Demo_DBEntities())
+            {
+                List<Student> students = DBEntities.Students.ToList();
+                foreach (Student student in students)
+                {
+                    Console.WriteLine($"{student.LastName} {student.FirstName}");
+                }
+            }
+        }
+    }
+}
